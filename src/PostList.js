b7fa12c39d630d8a2e76166571b556b09531d369
julianval20 <jevalenciav@udistrital.edu.co>
2025-04-29 07:30:35 -0500

    Add files via upload

--- src/PostList.js
+++ src/PostList.js
@@ -7,15 +7,31 @@ function PostList(){
         setPosts(posts.filter(post=>post.id !==id))
     }
 
+    const handleAdd = () =>{
+        console.log("Adding...");
+        const newId = posts.length > 0 ? posts[posts.length - 1].id + 1 : 1;
+        const newPost = {
+          id: newId,
+        };
+        setPosts([...posts, newPost]);
+
+        
+    }
+
     const [posts, setPosts] = useState([
         {"id": 1, "edad": 5, "nombre": "Juan"},
         {"id": 2, "edad": 7, "nombre": "María"},
         {"id": 3, "edad": 8, "nombre": "Ana"},
         {"id": 4, "edad": 9, "nombre": "Carlos"}
     ]);
-
-    return(
-        posts.map((post, index)=><App key={index} post={post} onDelete={handleDelete}/>)
-    )
+    
+    return (
+        <>
+          {posts.map((post) => (
+            <App key={post.id} post={post} onDelete={handleDelete} />
+          ))}
+          <button onClick={handleAdd}>Añadir</button>
+        </>
+      );
 }
 export default PostList
